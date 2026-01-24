f8019525d7b017253287c161ecf2a1d1b28294e2
haripriya1227 <army122003@gmail.com>
2026-01-24 14:47:28 +0530

    Fix server index setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,34 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import { ConnectDB } from './'
+import { ConnectDB } from './config/ConnectDB.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors"
+
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+app.use(core({
+    origin: process.env.CLIENT_URL,
+    Credentials: true,
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, async () => {
+    await ConnectDB()
+    console.log(`Server running on http:localhost:${process.env.PORT}`);
+})
